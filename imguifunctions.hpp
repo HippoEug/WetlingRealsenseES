e036dee68f36199ff7cf93d4084340e6cc680a1e
Eugene <efoo10@gmail.com>
2018-11-16 16:33:03 +0800

    Update imguifunctions.hpp

--- imguifunctions.hpp
+++ imguifunctions.hpp
@@ -3,6 +3,6 @@
 #include "imgui\imgui.h"
 
 namespace ImGuiFunctions {
-	void menuGUI(bool& show_color_camera, bool& show_depth_camera, bool& camera_button, bool& coordinates_button, bool& CSV_button);
+	void menuGUI(bool& show_color_camera, bool& show_depth_camera, bool& camera_button_png, bool& camera_button_rosbag, bool& convert_bag_button, bool& coordinates_button);
 	void rosbagGUI();
 }

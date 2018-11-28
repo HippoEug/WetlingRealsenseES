1c99bde97e670fb36b477d497ab8f1b90431b995
Eugene <efoo10@gmail.com>
2018-11-28 11:35:06 +0800

    Update imguifunctions.hpp

--- imguifunctions.hpp
+++ imguifunctions.hpp
@@ -4,5 +4,5 @@
 
 namespace ImGuiFunctions {
 	void menuGUI(bool& show_color_camera, bool& show_depth_camera, bool& camera_button_png, bool& camera_button_rosbag, bool& convert_bag_button, bool& coordinates_button);
-	void rosbagGUI(bool& rosbag_menu_display_donechar, char buf1[64]);
+	void rosbagGUI(bool& rosbag_menu_display_donechar, char bagFileBuffer[64], char newFileBuffer[64]);
 }

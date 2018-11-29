3a3eb3e808b4c8090fadbeb6c60ab5cb047bb4e5
Eugene <efoo10@gmail.com>
2018-11-29 16:28:48 +0800

    Update imguifunctions.hpp

--- imguifunctions.hpp
+++ imguifunctions.hpp
@@ -3,6 +3,8 @@
 #include "imgui\imgui.h"
 
 namespace ImGuiFunctions {
-	void menuGUI(bool& show_color_camera, bool& show_depth_camera, bool& camera_button_png, bool& camera_button_rosbag, bool& convert_bag_button, bool& coordinates_button);
-	void rosbagGUI(bool& rosbag_menu_display_donechar, char bagFileBuffer[64], char newFileBuffer[64]);
+	void menuGUI(bool& show_color_camera, bool& show_depth_camera, bool& camera_button_png, bool& camera_button_rosbag, bool& convert_bag_button, bool& coordinates_button, bool& import_csv_button);
+	void rosbagGUI(bool& rosbag_menu_display_done, char bagFileBuffer[64], char newFileBuffer[64]);
+	void csvGUI(bool& csv_menu_display_done, char csvFileBuffer[64]);
+	void opencvGUI(bool& opencv_menu_display_done, char opencvFileBuffer[64]);
 }

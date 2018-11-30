62987eb817c2e8fcfd76a0ccd9be60af7abe7fc5
Eugene <efoo10@gmail.com>
2018-11-30 14:44:17 +0800

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -14,20 +14,27 @@
 
 #include "displayfunctions.hpp" // For creating GLFW Window
 #include "imguifunctions.hpp" // For drawing ImGui menus
+#include "imguitheme.hpp"  // For changing ImGui themes
 #include "opencvfunctions.hpp" // For selecting points on an image and getting coordinates
-#include "converter.hpp"
-#include "converter-csv.hpp"
-#include "converter-png.hpp"
-#include "csvmanager.hpp"
-#include "imguitheme.hpp"
+#include "converter.hpp" // For converting .bag files
+#include "converter-csv.hpp" // For converting .bag to .csv files
+#include "converter-png.hpp" // For converting .bag to .png files
+#include "csvmanager.hpp" // For managing 921,600 (x, y, z) points
 
 int main(int argc, char* argv[]) {
 	// OpenGL + GLFW + ImGui Declarations
 	window_rs window; // Create window object from Class window_rs
 	window.onCreate(1280, 720, "WetlingRealsenseES V2.4"); // Creates a 1280x720 OpenGL window
-	ImGui_ImplGlfw_Init(window, true); // Initalize ImGui
-	imguithemes::windowsTheme();
+	ImGui_ImplGlfw_Init(window, true); // Initalize ImGui with callbacks enabled
 
+	imguithemes::windowsTheme(); // Changing ImGui theme values
+	ImGuiIO& io = ImGui::GetIO();
+	io.Fonts->Clear();
+	ImFont* font_small = io.Fonts->AddFontFromFileTTF("C:\\Windows\\Fonts\\segoeui.ttf", 19.0f);
+	font_small->DisplayOffset.y -= 1;
+	ImFont* font_large = io.Fonts->AddFontFromFileTTF("C:\\Windows\\Fonts\\segoeui.ttf", 25.0f);
+	font_large->DisplayOffset.y -= 1;
+	
 	// Realsense Declarations
 	texture_rs rgbPreview, depthPreview;
 	rs2::frameset data;
@@ -37,20 +44,20 @@ int main(int argc, char* argv[]) {
 	rs2::hole_filling_filter holeFilter;
 	std::vector<std::shared_ptr<rs2::tools::converter::converter_base>> convertToCSV;
 	std::vector<std::shared_ptr<rs2::tools::converter::converter_base>> convertToPNG;
-	auto pipe = std::make_shared<rs2::pipeline>();
+	auto pipe = std::make_shared<rs2::pipeline>(); // Realsense pipeline declaration
 
 	// Realsense Camera State Management
-	bool isCameraOn = false;
-	bool turnCameraOnRequest = false;
+	bool isCameraOn = false; // Bool to check if Camera is on at any given point in time
+	bool turnCameraOnRequest = false; // Bool to set when camera is needed to be on for various functions
 
 	// Realsense Pipeline Configurations
-	rs2::config convertConfig;
+	rs2::config convertConfig; // Pipeline configuration declaration when converting .bag files
 
-	rs2::config standardConfig;
+	rs2::config standardConfig; // Pipeline configuration declaration when performing normal streaming
 	standardConfig.enable_stream(RS2_STREAM_COLOR, -1, 1280, 720, rs2_format::RS2_FORMAT_RGB8, 0);
 	standardConfig.enable_stream(RS2_STREAM_DEPTH, -1, 1280, 720, rs2_format::RS2_FORMAT_Z16, 0);
 
-	rs2::config recordConfig;
+	rs2::config recordConfig; // Pipeline configuration declaration when performing .bag recording
 	recordConfig.enable_record_to_file("record.bag");
 	recordConfig.enable_stream(RS2_STREAM_COLOR, -1, 1280, 720, rs2_format::RS2_FORMAT_RGB8, 0);
 	recordConfig.enable_stream(RS2_STREAM_DEPTH, -1, 1280, 720, rs2_format::RS2_FORMAT_ANY, 0);
@@ -65,67 +72,65 @@ int main(int argc, char* argv[]) {
 	bool show_color_camera = false; // Stream Color
 	bool show_depth_camera = false; // Stream Depth
 	bool camera_button_png = false; // Capture Color & Depth PNG
-	bool camera_button_rosbag = false; // Capture 1s ROSBAG TODO
-	bool convert_bag_button = false; // Generate CSV File with ROSBAG TODO
+	bool camera_button_rosbag = false; // Capture 1s ROSBAG
+	bool convert_bag_button = false; // Generate CSV File with ROSBAG
 	bool coordinates_button = false; // Select Coordinates in a PNG
-	bool import_csv_button = false;
+	bool import_csv_button = false; // Import 921,600 points for volume calculation
 
-	bool rosbag_menu_display_done = false;
-	bool csv_menu_display_done = false;
-	bool opencv_menu_display_done = false;
+	bool rosbag_menu_display_done = false; // Bool condition to manage closing of Rosbag menu
+	bool csv_menu_display_done = false; // Bool condition to manage closing of CSV menu
+	bool opencv_menu_display_done = false; // Bool condition to manage closing of OpenCV menu
 
 	// ImGui Display Variables
-	char bagFileBuffer[64] = "";
-	char newFileBuffer[64] = "";
-	char csvFileBuffer[64] = "";
-	char opencvFileBuffer[64] = "";
+	char bagFileBuffer[64] = ""; // Buffer to store .bag file name
+	char newFileBuffer[64] = ""; // Buffer to store file name to be saved
+	char csvFileBuffer[64] = ""; // Buffer to store .csv file name
+	char opencvFileBuffer[64] = ""; // Buffer to store .png file name for OpenCV
 
-	while (window) {
+	while (window) { // From window_rs class, calls operator bool() every loop
 		ImGui_ImplGlfw_NewFrame(1);
-		ImGuiFunctions::menuGUI(show_color_camera, show_depth_camera, camera_button_png, camera_button_rosbag, convert_bag_button, coordinates_button, import_csv_button);
+		ImGuiFunctions::menuGUI(font_large, font_small, show_color_camera, show_depth_camera, camera_button_png, camera_button_rosbag, convert_bag_button, coordinates_button, import_csv_button); // Creates and displays main menu
 
-		if (show_color_camera == true || show_depth_camera == true) {
-			turnCameraOnRequest = true;
+		// Checks if streaming condition is set
+		if (show_color_camera == true || show_depth_camera == true) { // Check if color or depth streaming is enabled
+			turnCameraOnRequest = true; // Sets request to turn camera on
 
 			if (isCameraOn == true && turnCameraOnRequest == true) { // If camera needs to be on, and is already on, do nothing
-				// Do nothing
+				std::cout << "Camera Already On!" << std::endl;
 			}
-			else if (isCameraOn == false && turnCameraOnRequest == true) {
-				pipe->start(standardConfig);
-				isCameraOn = true;
+			else if (isCameraOn == false && turnCameraOnRequest == true) { // If camera needs to be on, but it is not on, start pipeline
+				pipe->start(standardConfig); // Start Realsense pipeline with standard configuration
+				isCameraOn = true; // Sets camera condition as on
 			}
 
-			data = pipe->wait_for_frames().apply_filter(holeFilter);
-			color = data.get_color_frame();
-			depth = color_map.process(data.get_depth_frame());
+			data = pipe->wait_for_frames().apply_filter(holeFilter); // Wait for frames for pipeline and apply hole filling filter
+			color = data.get_color_frame(); // Get color frame from data frameset
+			depth = color_map.process(data.get_depth_frame()); // Get depth frame from data frameset
 
-			if (show_color_camera) {
+			if (show_color_camera) { // If color streaming is enabled, render color frames
 				rgbPreview.render(color, { 640, 0, window.width() / 2, window.height() / 2 });
 			}
 
-			if (show_depth_camera) {
+			if (show_depth_camera) { // If depth streaming is enabled, render depth frames
 				depthPreview.render(depth, { 640, 360, window.width() / 2, window.height() / 2 });
 			}
 		}
-		else {
+		else { // If color or depth streaming is not enabled, set request to turn camera on to false
 			turnCameraOnRequest = false;
 		}
 
-		if (camera_button_png) {
-			turnCameraOnRequest = true;
+		if (camera_button_png) { // Set if camera png capture is pressed
+			turnCameraOnRequest = true; // Sets request to turn camera on
 
 			if (isCameraOn == true && turnCameraOnRequest == true) { // If camera needs to be on, and is already on, do nothing
-				// Do nothing
+				std::cout << "Camera Already On!" << std::endl;
 			}
-			else if (isCameraOn == false && turnCameraOnRequest == true) {
-				pipe->start(standardConfig);
-				isCameraOn = true;
-			}
-			else if (isCameraOn == true && turnCameraOnRequest == false) {
-				pipe->stop();
+			else if (isCameraOn == false && turnCameraOnRequest == true) { // If camera needs to be on, but it is not on, start pipeline
+				pipe->start(standardConfig); // Start Realsense pipeline with standard configuration
+				isCameraOn = true; // Sets camera condition as on
 			}
 
-			for (auto i = 0; i < 10; ++i) { // Capture 10 frames for camera hardware to settle
+			for (auto i = 0; i < 20; ++i) { // Capture 20 frames for camera hardware to settle
 				pipe->wait_for_frames();
 			}
 
@@ -174,7 +179,7 @@ int main(int argc, char* argv[]) {
 		}
 
 		if (convert_bag_button) { // TODO: Fix Realsense Pipeline
-			ImGuiFunctions::rosbagGUI(rosbag_menu_display_done, bagFileBuffer, newFileBuffer);
+			ImGuiFunctions::rosbagGUI(rosbag_menu_display_done, bagFileBuffer, newFileBuffer); // Creates and displays rosbag menu
 
 			if (rosbag_menu_display_done) {
 				convertToCSV.push_back(std::make_shared<rs2::tools::converter::converter_csv>(newFileBuffer));
@@ -229,7 +234,7 @@ int main(int argc, char* argv[]) {
 		}
 
 		if (coordinates_button) {
-			ImGuiFunctions::opencvGUI(opencv_menu_display_done, opencvFileBuffer);
+			ImGuiFunctions::opencvGUI(opencv_menu_display_done, opencvFileBuffer); // Creates and displays OpenCV menu
 
 			if (opencv_menu_display_done) {
 				selectCoordinates(opencvFileBuffer);

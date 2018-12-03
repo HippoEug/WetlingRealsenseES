7efbccf39ea75244dd23060ae8ee425515bed33f
Eugene <efoo10@gmail.com>
2018-12-03 14:06:45 +0800

    Update opencvfunctions.hpp

--- opencvfunctions.hpp
+++ opencvfunctions.hpp
@@ -1,4 +1,6 @@
 #pragma once
 
-void selectCoordinates(char opencvFileBuffer[64]);
-static void onMouse(int event, int x, int y, int, void* imgptr);
+namespace OpenCVFunctions {
+	void selectCoordinates(char opencvFileBuffer[64]);
+	static void onMouse(int event, int x, int y, int, void* imgptr);
+}
